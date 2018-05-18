48c279fff957dc9a356ccfa0c504624aacd1ec7d
CongHuy <huynhconghuy92@gmail.com>
2018-05-18 17:31:04 +0700

    --

---
+++ app/src/main/java/com/conghuy/example/TestModel.java
@@ -0,0 +1,11 @@
+package com.conghuy.example;
+
+public class TestModel {
+    public int position;
+    public String title;
+
+    public TestModel(int position, String title) {
+        this.position = position;
+        this.title = title;
+    }
+}
